79332350c6c082cddc0588c7222bad7353a80083
jaydeep099 <132276741+jaydeep099@users.noreply.github.com>
2024-01-24 23:04:41 +0530

    Bharat-clock first commit

---
+++ Bharat-Clock/src/App.jsx
@@ -0,0 +1,16 @@
+import AppName from "./components/AppName"
+import ClockMoto from "./components/ClockMoto"
+import CurrentTime from "./components/CurrentTime"
+
+function App() {
+
+  return (
+    <center>
+      <AppName />
+      <ClockMoto />
+      <CurrentTime />
+    </center>
+  )
+}
+
+export default App
